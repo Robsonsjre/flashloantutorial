d3ff55d54199e10d243447f9bee131bbed24ada0
Robsonsjre <robsonsjre@gmail.com>
2020-04-13 11:45:04 -0300

    added builded contracts

--- contracts/flashloan.sol
+++ contracts/flashloan.sol
@@ -5,48 +5,32 @@ import '../aave/ILendingPool.sol';
 import "../aave/FlashLoanReceiverBase.sol";
 import "../aave/ILendingPoolParametersProvider.sol";
 
-contract FlashLoan is FlashLoanReceiverBase {
+//DAI ADDRESS: 0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD
+// PROVIDER ADDRESS: 0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5
+
+contract MyfirstFlashLoan is FlashLoanReceiverBase(address(0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5)) {
     ILendingPoolAddressesProvider provider;
-    address dai;
-
-    constructor(address _dai, address _provider) public {
-        dai = _dai;
-        provider = ILendingPoolAddressesProvider(_provider);
+ 
+    function flashLoan(uint256 newamount, address _token) external {
+        bytes memory _params = "0x0";
+        address exchangeAddress = addressesProvider.getLendingPool();
+         ILendingPool exchange = ILendingPool(exchangeAddress);
+         
+         exchange.flashLoan(address(this), _token, newamount, _params);
     }
-
-    function startLoan(uint amount, bytes calldata _params) external {
-        address lendingPoolAddress = provider.getLendingPool();
-        ILendingPool lendingPool = ILendingPool(lendingPoolAddress);
-        lendingPool.flashLoan(address(this), dai, amount, _params);
-    }
-
+    
     function executeOperation(
-        address _reserve,
-        uint256 _amount,
-        uint256 _fee,
-        bytes calldata _params
-    )
-        external
-    {
-        require(_amount <= getBalanceInternal(address(this), _reserve), "Invalid balance, was the flashLoan successful?");
-
-        //
-        // do your thing here
-        //
-
-        // Time to transfer the funds back
-        uint totalDebt = _amount.add(_fee);
-        transferFundsBackToPoolInternal(_reserve, totalDebt);
+        address _reserve, 
+        uint256 _amount, 
+        uint256 _fee, 
+        bytes calldata _params) external {
+            
+        // ARBRITAGEM
+        // LIQUIDATION VAULT MAKER
+        // INTERST SWAP ENTRE LENDING PROTOCOLS
+        // TOKEN SWAP
+        
+      uint256 totalDebt = _amount.add(_fee);
+      transferFundsBackToPoolInternal(_reserve, totalDebt);
     }
-
-    function checkAaveFees() external returns(uint256, uint256) {
-        address parametersProviderAddress = provider.getLendingPoolParametersProvider();
-        ILendingPoolParametersProvider parametersProvider = ILendingPoolParametersProvider(parametersProviderAddress);
-
-        (uint256 totalFeeBips, uint256 protocolFeeBips) = parametersProvider
-            .getFlashLoanFeesInBips();
-
-        return(totalFeeBips, protocolFeeBips);
-    }
-
 }
